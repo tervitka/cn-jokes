5e60d6d1ebaaad11841dce46885068cfb1e75faf
Matej Zelenka <Matej.Zel@seznam.cz>
2022-06-06 13:58:09 +0200

    Chakra init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import './css/App.css';
 import { api } from './api';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Heading, SimpleGrid } from '@chakra-ui/react';
 
 function App() {
 
   api.get('/random').then(response => console.log(response.data.value));
 
   return (
-    <Container>
-      <Row>
-        <Col>1</Col>
-        <Col>2</Col>
-        <Col>3</Col>
-      </Row>
-    </Container>
+    <SimpleGrid>
+      <Heading>
+        Ahoj
+      </Heading>
+    </SimpleGrid>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
