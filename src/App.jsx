2e41d976f53c53ea9ca4a54c407e4900b2104d06
Matej Zelenka <Matej.Zel@seznam.cz>
2022-06-06 11:22:06 +0200

    React Bootstrap init

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import './css/App.css';
 import { api } from './api';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
 
   api.get('/random').then(response => console.log(response.data.value));
 
   return (
-    <div className="container">
-      <div className='row'>
-        <div className='col-4'>
-          <p>1</p>
-        </div>
-        <div className='col-4'>
-          <p>2</p>
-        </div>
-        <div className='col-4'>
-          <p>3</p>
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col>1</Col>
+        <Col>2</Col>
+        <Col>3</Col>
+      </Row>
+    </Container>
   );
 }
 
