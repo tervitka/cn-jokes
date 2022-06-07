c2a8b4e3a07264299a119d33aaf2faf502804576
Matej Zelenka <Matej.Zel@seznam.cz>
2022-06-07 14:07:52 +0200

    Custom themes init

---
+++ src/theme.js
@@ -0,0 +1,34 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.400',
+        color: 'white',
+        fontFamily: 'Source Sans Pro, cursive'
+      },
+    },
+  },
+  colors: {
+    orange: {
+      100: '#D68D00'
+    },
+    green: {
+      100: '#008505'
+    },
+    gray: {
+      100: '#D9D9D9',
+      200: '#808080'
+    }
+  },
+  components: {
+    Heading: {
+      baseStyle: {
+        fontFamily: "Londrina Solid, cursive",
+      }
+    }
+  }
+})
+
+export default theme;
