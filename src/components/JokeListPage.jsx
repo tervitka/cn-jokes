2d4131f263ea41bae55a9c2c9d384e7c643964ea
Matej Zelenka <Matej.Zel@seznam.cz>
2022-06-30 08:41:33 +0200

    Final commit

--- src/components/JokeListPage.jsx
+++ src/components/JokeListPage.jsx
@@ -1,5 +1,5 @@
 import {useState, useCallback, useEffect} from "react";
-import {Container, Flex, Heading, Button, Center} from "@chakra-ui/react";
+import {Container, Flex, Heading } from "@chakra-ui/react";
 import {api} from "../api";
 import {JokeCard} from "./JokeCard";
 import {ParametersForm} from "./ParametersForm";
@@ -62,24 +62,6 @@ export function JokeListPage() {
           <JokeCard joke={joke} jokeCategory={jokeCategory}/>
         </Flex>
       </Container>
-
-      <Center>
-        <Button
-          boxShadow="1px 4px 4px 1px green"
-          bg="white"
-          color="green"
-          position="center"
-          height="3rem"
-          fontSize="1.5rem"
-          _hover={{
-            bg: "green",
-            color: "white",
-            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-          }}
-        >
-          go back up
-        </Button>
-      </Center>
     </>
   );
 }
